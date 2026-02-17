38c43ce3465c7598255c90d55643de009f36f044
M. Abdullah <m.abdullah.pk68@gmail.com>
2026-02-17 11:43:23 +0500

    Add project files.

---
+++ Data/AppDBContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SMS_Web_App_Version.Models;
+
+namespace SMS_Web_App_Version.Data
+{
+    public class AppDBContext : DbContext
+    {
+        public AppDBContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        DbSet<Student> students { get; set; }
+    }
+}
---
+++ Models/Student.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SMS_Web_App_Version.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Name { get; set; }
+
+        [Required]
+        [Range(8, 60)]
+        public int Age { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string EmailAddress { get; set; }
+
+        public string cource { get; set; }
+    }
+}
